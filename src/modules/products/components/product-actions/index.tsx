442805683e42e9a80abc2b4d5dad10c6a1f3b702
David F <davidrsferreira@gmail.com>
2024-08-30 01:15:51 +0100

    fix build

--- src/modules/products/components/product-actions/index.tsx
+++ src/modules/products/components/product-actions/index.tsx
@@ -17,6 +17,13 @@ type ProductActionsProps = {
   region: Region
 }
 
+export type PriceType = {
+  calculated_price: string
+  original_price?: string
+  price_type?: "sale" | "default"
+  percentage_diff?: string
+}
+
 export default function ProductActions({
   product,
   region,
