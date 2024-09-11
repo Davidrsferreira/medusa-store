745c636605deab2acd87f650a5073448162f1def
David F <davidrsferreira@gmail.com>
2024-09-11 11:39:25 +0100

    changed page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "styles/globals.css"
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
+      <head>
+        <title>Back2Brazil Shop</title>
+      </head>
       <body>
         <main className="relative">{props.children}</main>
       </body>
