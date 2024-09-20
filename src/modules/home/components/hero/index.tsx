ffdf2ae99cc88f34a66edda5289ad9882d90f8dc
David F <davidrsferreira@gmail.com>
2024-09-20 11:29:44 +0100

    added hero img

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,9 +1,17 @@
 import { ShoppingBag } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+import { Button } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[45vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div
+      className="h-[45vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle"
+      style={{
+        backgroundImage: "url('/images/wide.jpeg')",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center"
+      }}
+    >
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <a href="/store">
           <Button variant="secondary">
